3c36bf100076580deffd2e6e2c3060650c635f41
Alison Zhang <alisonzthu@gmail.com>
2018-06-12 13:53:17 -0700

    added viewmodel file structures

---
+++ app/src/main/java/com/example/macstudio/basicsample/viewmodel/ProductListViewModel.java
@@ -0,0 +1,12 @@
+package com.example.macstudio.basicsample.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class ProductListViewModel extends AndroidViewModel{
+
+    public ProductListViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
